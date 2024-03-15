b7386b6cd49f2388c34f34ec2755ba117da58017
hunpark <gnsals64@gmail.com>
2024-03-15 18:51:26 +0900

    fix

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,20 +4,21 @@ service mariadb start
 
 # sleep 5
 
-# mysql_secure_installation << EOF
-# n
-# ${DB_ROOT_PASSWORD}
-# ${DB_ROOT_PASSWORD}
-# y
-# n
-# n
-# n
-# n
-# EOF
+mysql_secure_installation << EOF
+n
+${DB_ROOT_PASSWORD}
+${DB_ROOT_PASSWORD}
+y
+n
+n
+n
+n
+EOF
 
-# mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-# mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
-# mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
-# mariadb -e "FLUSH PRIVILEGES;"
-# mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
-# exec mariadbd
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "FLUSH PRIVILEGES;"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PWD}';"
+mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+exec mariadbd
