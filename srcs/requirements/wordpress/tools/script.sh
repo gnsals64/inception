ba6ff34ead3a5d779fab7dea90c15d472feb58cf
gnsals64 <gnsals64@gmail.com>
2024-03-16 03:36:18 +0900

    fix

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -7,7 +7,7 @@ if [ ! $(wp core is-installed --path=/var/www/html --allow-root)]; then
   echo "make config"
 
   sleep 5
-  wp core install--url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWORD} \
+  wp core install --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWORD} \
                 --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
   echo "core install test"
   wp user create ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
