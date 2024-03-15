6a5e7fae2beca08e17defa72b67aa5d39c9617e3
gnsals64 <gnsals64@gmail.com>
2024-03-16 03:13:26 +0900

    fix

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,10 +2,22 @@
 
 service mariadb start
 sleep 5
+
+#DB 만들기
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+#user 만들기
 mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
+
+#DB 권한 부여하기
 mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+
+#설정 적용
 mariadb -e "FLUSH PRIVILEGES;"
+
+#root의 비밀번호 설정 (비밀번호 설정시 root 계정 외에는 접속 불가)
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
+
+#mysql 껐다 켜기
 mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 exec mariadbd
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,7 +1,7 @@
 if [ ! $(wp core is-installed --path=/var/www/html --allow-root)]; then
   wp core download --allow-root
-  wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWD} --dbhost=mariadb:3306 --allow-root
-  wp core install--url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWD} \
+  wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWORD} --dbhost=mariadb:3306 --allow-root
+  wp core install--url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWORD} \
                 --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
-  wp user create ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWD} --allow-root
+  wp user create ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD} --allow-root
 fi
