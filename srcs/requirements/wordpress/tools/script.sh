a4ab7e238b677254f2f81cd78337e9493e5a1d42
hunpark <gnsals64@gmail.com>
2024-03-15 18:15:02 +0900

    fix

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,5 +1,5 @@
 if [ ! $(wp core is-installed --path=/var/www/html --allow-root)]; then
-  wp core download 
+  wp core download --allow-root
   wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWD} --dbhost=mariadb:3306 --allow-root
   wp core install--url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWD} \
                 --admin_email=${WP_ADMIN_EMAIL} --skip-email --allow-root
