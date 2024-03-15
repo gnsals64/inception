8e107e3522a176e50ae7b4992107c4e9b97aca00
hunpark <gnsals64@gmail.com>
2024-03-15 17:57:32 +0900

    fix

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,7 @@
 if [ ! $(wp core is-installed --path=/var/www/html)]; then
-  wp core download --allow-root
-  wp config create --allow-root --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWD} --dbhost=mariadb:3306
-  wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWD} \
+  wp core download 
+  wp config create --dbname=${DB_NAME} --dbuser=${DB_USER} --dbpass=${DB_PASSWD} --dbhost=mariadb:3306
+  wp core install--url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_NAME} --admin_password=${WP_ADMIN_PASSWD} \
                 --admin_email=${WP_ADMIN_EMAIL} --skip-email
-  wp user create --allow-root ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWD}
+  wp user create ${WP_USER_NAME} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWD}
 fi
-
-php-fpm7.4 -F
