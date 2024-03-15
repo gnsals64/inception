a585118640bf19afdda8729743ed172c682a6868
gnsals64 <gnsals64@gmail.com>
2024-03-16 02:27:10 +0900

    fix

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
 service mariadb start
-
-# sleep 5
-
-mysql_secure_installation << EOF
-n
-${DB_ROOT_PASSWORD}
-${DB_ROOT_PASSWORD}
-y
-n
-n
-n
-n
-EOF
-
+sleep 5
 mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mariadb -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASSWORD}';"
 mariadb -e "FLUSH PRIVILEGES;"
 mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
-mysqladmin -u root -p${DB_ROOT_PASSWORD} shutdown
+mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
 exec mariadbd
